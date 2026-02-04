f074c54ece8a99bd28fa2e1b9cc18da486f64520
Anas Quatra <anas.quatra@enseirb-matmeca.fr>
2026-02-04 15:06:40 +0100

    tweak

--- eslint.config.js
+++ eslint.config.js
@@ -11,8 +11,10 @@ export default defineConfig([
   { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
   {
 		rules: {
-			"no-unused-vars": "warn",
+			"no-unused-vars": "off",
+     "@typescript-eslint/no-unused-vars": "warn",
 			"no-undef": "warn",
+      "vue/multi-word-component-names": "off",
 		},
 	},
 ]);
