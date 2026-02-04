f857f5bcce12e26666e6131afeaca77e6a77e5c8
Anas Quatra <anas.quatra@enseirb-matmeca.fr>
2026-02-04 14:42:17 +0100

    eslint config

--- pipelines/ppl.sh
+++ pipelines/ppl.sh
@@ -8,3 +8,12 @@ pnpm nuxt typecheck
     # <GameScoreDisplay label1="Best" :value="bestScore" />
     # au lieu de :
     # <GameScoreDisplay label="Best" :value="bestScore" />
+
+# Exécuter eslint pour qu'il analyse statiquement les fichiers Vue et TypeScript: s'assurer qu'il n'y a pas d'erreur
+ pnpm eslint app/ --ext .js,.ts,.vue
+
+# Ajouter ou modifier du code pour qu'ESLint remonte une erreur. Constater que l'IDE remonte bien l'erreur
+
+# Vérifier que l'erreur est bien remontée également en exécutant ESLint
+
+# Enlever la modification et ajouter l'étape d'analyse statique de code au script
